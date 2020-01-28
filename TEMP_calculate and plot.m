5665bb9efbef733bff6c7db0717dafa1e2371de4
francistianlal <60010268+francistianlal@users.noreply.github.com>
2020-01-28 16:45:42 +0100

    Create TEMP_calculate and plot.m

---
+++ TEMP_calculate and plot.m
@@ -0,0 +1,24 @@
+%start with clearing
+clc;clear;
+% This program is intended for the quick calcualtion and ploting
+% It contains three sections: Unit Conversion Definition, Parameter Definition, Equation Definition,
+% Plotting preparation and plot & figure setting
+% The original model are for 2D plotting 
+
+%% Unit Conversion Definition
+Unit_A = ; %
+Unit_B = ; %
+
+%% Parameter Definition
+ 
+%% Equation Definition
+
+%% Plotting Definition
+
+%% Plot and figure setting
+
+plot( , , '');
+xlabel('');
+ylabel('');
+title('');
+
